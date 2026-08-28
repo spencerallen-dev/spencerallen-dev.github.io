b73b0daf0c085a75720d8d9d9e9e0379ac610955
Allen Spencer <allenspencer912@gmail.com>
2026-08-28 23:41:09 +1000

    Enforce permanent black background, remove theme.js, and add style.css v4 cachebuster

--- js/theme.js
+++
@@ -1,8 +0,0 @@
-// Permanent Dark Mode Enforcer
-(function() {
-  try {
-    localStorage.removeItem('theme');
-    document.documentElement.removeAttribute('data-theme');
-  } catch (e) {}
-})();
-
